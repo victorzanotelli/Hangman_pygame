90531286e0ea6ced59c3ef185bae94ea7442f27a
Victor <victor.anclin@epitech.eu>
2026-09-17 14:27:34 +0200

    core hangman game mechanic add still no display

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
-import pygame  #main structure for pygame :                                     1.Check event
-#                                                                               2.Update game state         
-pygame.init()#                                                                  3.Draw everything
-#                                                                               4.Display.flip()
-#create the window for the game and renamed it                                  5.Repeat
+import pygame
+# Game loop:
+# 1. Check events
+# 2. Update game state
+# 3. Draw everything
+# 4. Display the result
+# 5. Repeat
+                                                                                  
+pygame.init()                                                                
+                                                                              
+# Create the game window and store it in the "screen" variable                 
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Hangman")
 
-#Defining the secret word
+# Defining the secret word
 word = "PYTHON"
 
-#Create the hidden veersion of the secret word
-hidden_word = "_ " * len(word)
+# Create the hidden version of the secret word
+hidden_word = ["_"] * len(word)
 
-#Create a font (more or less defining the detail for the display of the text)
+# Create a font and set its size
 font = pygame.font.Font(None, 50)
 
 running = True
 
 while running:
 
-    #Handle events
+    # Handle events
     for event in pygame.event.get():
+
+        #Handle the event "quiting the game"
         if event.type == pygame.QUIT:
             running = False
 
-    #Clearing the screen
+        # Handle the event "wich letter from keyboard is pressed"
+        if event.type == pygame.KEYDOWN:
+            letter = event.unicode.upper()
+
+            print(letter)
+
+            if letter in word :
+                print("Correct!")
+            else :
+                print("Wrong!")
+
+    # Clearing the screen
     screen.fill("white")
 
-    #Create the text
+    # Create the text
     text = font.render(hidden_word, True,"black")
 
-    #Draw the text 
+    # Draw the text 
     screen.blit(text,(300,300))
 
-    #Show on the screen
+    # Show on the screen
     pygame.display.flip()
 
 pygame.quit()
